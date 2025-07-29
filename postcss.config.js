3d294f9841f66bbde2b0bb16583ad8e6c9ef664c
Eduardo Di Fermo <89153688+edudifermo@users.noreply.github.com>
2025-07-29 10:45:22 -0300

    reconfigurando postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-      plugins: {
-        tailwindcss: {},
-      },
-    };
-
-    export default config
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
